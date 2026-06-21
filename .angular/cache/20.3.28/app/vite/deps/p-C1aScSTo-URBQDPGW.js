a995fdc84a55131239473f72914e334278a18dad
Murilo Muller (Mac) <murilomuller@protonmail.com>
2026-06-21 18:15:34 -0300

    feat(vite): add SSR metadata and package configuration
    - Created _metadata.json for SSR dependencies with hash and chunk information.
    - Added package.json to define the module type for SSR dependencies.

---
+++ .angular/cache/20.3.28/app/vite/deps/p-C1aScSTo-URBQDPGW.js
@@ -0,0 +1,13 @@
+import {
+  a,
+  l
+} from "./chunk-ZLJPHCPN.js";
+import "./chunk-MTYFBTPK.js";
+import "./chunk-FBOO75ZN.js";
+import "./chunk-Q6ZGL456.js";
+import "./chunk-5ZNTDABU.js";
+import "./chunk-WDMUDEB6.js";
+export {
+  l as iosTransitionAnimation,
+  a as shadow
+};
